7ad0d42e5abcc11f220ed6329f09a14a6bdd1ab9
Nilothpal Pillai <Nilothpal.Pillai@gmail.com>
2022-09-04 03:38:37 +0530

    .env and Migrations

--- videoApp/models.py
+++ videoApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, blank=True)
+    published_at = models.CharField(max_length=30)
+    channel_title = models.CharField(max_length=200)
+    thumbnail_default = models.URLField(max_length=200, blank=True)
+    thumbnail_medium = models.URLField(max_length=200, blank=True)
+    thumbnail_high = models.URLField(max_length=200, blank=True)
--- videoFeeder/settings.py
+++ videoFeeder/settings.py
@@ -10,17 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r*c##r=ejiw5w@u*jdaj(gemm--c76w*$)*fl677@fgyr&eq8^'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'videoApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'videoFeeder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USERNAME'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
